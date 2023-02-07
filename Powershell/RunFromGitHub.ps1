be5573a141dbc1f679755c2c38de03faa628d351
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2023-02-07 15:09:59 -0500

    Update RunFromGitHub.ps1

--- Powershell/RunFromGitHub.ps1
+++ Powershell/RunFromGitHub.ps1
@@ -1,5 +1,6 @@
 #Go to github repo and open the script, then hit the raw button to get to this link
-$RAW = "https://raw.githubusercontent.com/landonis/WindowsGuide/main/Powershell/RunFromGitHub.ps1"
+$_file = read-host -prompt 'Input file name from github powershell directory'
+$RAW = "https://raw.githubusercontent.com/landonis/WindowsGuide/main/Powershell/" + $_file
 
 #Invoke a web request to get the script's text
 $script = Invoke-WebRequest $RAW
