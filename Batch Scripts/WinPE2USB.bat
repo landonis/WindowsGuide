9ec7bb5505786d2ea264d71f14abb506c3dbf449
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2023-02-07 12:49:00 -0500

    Add files via upload

---
+++ Batch Scripts/WinPE2USB.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal enabledelayedexpansion
+echo WinPE install - Make sure to have the Windows Assessment and Deployment kit + WinPE add-ons installed from https://learn.microsoft.com/en-us/windows-hardware/get-started/adk-install
+call "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
+
+set _drive=C:\WinPE_amd64
+
+
+set /p _boot="USB drive letter: "
+
+start cmd.exe /c "C:\copype amd64 %_drive%" 
+
+MakeWinPEMedia /ufd /f %_drive% %_boot%
+
