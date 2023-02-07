7acb712478911246df1a9d3beca8a7052cd9ee86
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2023-02-07 12:01:11 -0500

    Add files via upload

---
+++ Batch Scripts/StringCleaner.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal enabledelayedexpansion
+echo String cleanup bat
+
+set REM=-;\;/;:
+echo Chars to remove: "%REM:;= %"
+echo %REM%
+set /p REMSpaces=Do you want to remove blank spaces (Y/N)?
+
+
+set /p _drive="string: "
+set out=!_drive!
+
+@for %%i in (%REM%) do (
+	@if not "!_drive:%%i=!"=="!_drive!" echo removing character %%i
+	set out=!out:%%i=!
+)
+@if not "!REMSpaces:Y=!"=="!RemSpaces!" (
+	@for %%c in (%out%) do (
+		set out=!out: =!
+	)
+)
+echo New string: %out%
