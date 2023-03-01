31c318fdadd6ec2ec835790853a59c898e9aab16
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2023-03-01 17:33:08 -0500

    Update runPowershellOnVM.ps1

--- Powershell/runPowershellOnVM.ps1
+++ Powershell/runPowershellOnVM.ps1
@@ -1,3 +1,9 @@
+#Set VMName to the name of the VM you are running the scripts on
 $session = New-PSSession -VMName "VMName"
-Copy-Item -Path "C:\Path\To\Script.ps1" -Destination "C:\Path\To\Script.ps1" -ToSession $session
-Invoke-Command -Session $session -FilePath "C:\Path\To\Script.ps1"
+
+#Copy Folder from Host machine to the Hyper-V Machine
+Copy-Item -Path "C:\Path\To\Script\Folder" -Destination "C:\Path\To\Script\Folder" -ToSession $session
+
+#Run the scripts on the session
+Invoke-Command -Session $session -FilePath "C:\Path\To\Script\Folder\run.ps1"
+#can run a powershell script pointing to bat files since it only will run powershell this way
