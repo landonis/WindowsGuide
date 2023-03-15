2610c51ecd8b5ae26f76a77dc0b1e1a262d00141
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2023-03-15 12:41:38 -0400

    Update ApplicationManagement.ps1

--- Powershell/ApplicationManagement.ps1
+++ Powershell/ApplicationManagement.ps1
@@ -1,11 +1,10 @@
 #collection of application management commands
 
+#Programs
+Get-Package | Where-Object {($_.ProviderName -eq 'Programs') -or ($_.ProviderName -eq 'msi')} | Select-Object Name
+
 #gives AppxPackages
 $AppxNames = Get-AppxPackage | select Name 
-
-
-#other apps
-$PackageNames = Get-Package | select Name
 
 #get apps via registry
 $InstalledSoftware = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
