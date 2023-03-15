8a7d66f092c918dfcc4d5ed5598891df0500735a
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2023-03-15 13:56:00 -0400

    Update ApplicationManagement.ps1

--- Powershell/ApplicationManagement.ps1
+++ Powershell/ApplicationManagement.ps1
@@ -6,5 +6,8 @@ Get-Package | Where-Object {($_.ProviderName -eq 'Programs') -or ($_.ProviderNam
 #gives AppxPackages
 $AppxNames = Get-AppxPackage | select Name 
 
+#Get Apps via WMI CIM_Product
+Get-WMIObject -class Win32_Product | Where-Object{$_.__SUPERCLASS -eq "CIM_Product"}
+
 #get apps via registry
 $InstalledSoftware = Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall"
