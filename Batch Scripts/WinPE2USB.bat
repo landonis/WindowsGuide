883c740c311cd0549df085faf0aff307db804f2d
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2023-09-08 10:27:15 -0400

    Update WinPE2USB.bat

--- Batch Scripts/WinPE2USB.bat
+++ Batch Scripts/WinPE2USB.bat
@@ -3,7 +3,7 @@ setlocal enabledelayedexpansion
 echo WinPE install - Make sure to have the Windows Assessment and Deployment kit + WinPE add-ons installed from https://learn.microsoft.com/en-us/windows-hardware/get-started/adk-install
 call "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit\Deployment Tools\DandISetEnv.bat"
 
-if not exists C:\WinPE_amd64 (copype amd64 C:\WinPE_amd64)
+if not exist "C:\WinPE_amd64" (copype amd64 C:\WinPE_amd64)
 set _drive=C:\WinPE_amd64
 
 
